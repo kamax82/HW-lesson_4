be5ef4413538348062569466d2f222b476161bb6
Maks Karpuk <kamax@atlex.ru>
2020-01-30 18:12:14 +0300

    Fixes by your recommendations

--- home_work/tests/test_dog.py
+++ home_work/tests/test_dog.py
@@ -33,29 +33,29 @@ def test_dog_random_mult_img():
     assert parsed_data.count('.jpg') == 1
 
 
-@pytest.mark.parametrize('multi', ['1', '49', '50', '51'])
-def test_dog_random_mult_img(multi):
-    '''Response should contain as many jpgs as given, bot not more than 50'''
+@pytest.mark.parametrize('multi', ['1', '10', '49', '50'])
+def test_dog_random_mult_img_positive(multi):
+    '''Response should contain as many jpgs as given. Sometimes test can fail, because answer contain less items, thet requested by 1 paint'''
 
     response = requests.get('https://dog.ceo/api/breeds/image/random/' + multi)
     parsed_data = response.text
-    if int(multi) > 50:
-        assert parsed_data.count('.jpg') == 50
-    else:
-        assert parsed_data.count('.jpg') == int(multi)
+    assert parsed_data.count('.jpg') == int(multi)
+
+
+@pytest.mark.parametrize('multi', ['51', '100'])
+def test_dog_random_mult_img_negative(multi):
+    '''Response should contain 50 jpgs. Sometimes test can fail, because answer contain less items, thet requested by 1 paint'''
+
+    response = requests.get('https://dog.ceo/api/breeds/image/random/' + multi)
+    parsed_data = response.text
+    assert parsed_data.count('.jpg') == 50
 
 
 @pytest.mark.parametrize('breed', ["afghan", "basset", "blood", "english", "ibizan", "walker"])
 def test_dog_sub_breed_img(breed):
     '''Response should contain individual path for each breed'''
-    true = 0
-    false = 0
 
     response = requests.get('https://dog.ceo/api/breed/hound/' + breed + '/images')
     parsed_data = response.text
-    if (parsed_data.count('.jpg') > 0) and (parsed_data.count('hound-' + breed) > 0):
-        true += 1
-    else:
-        false += 1
-
-    assert false == 0
+    assert parsed_data.count('.jpg') > 0
+    assert parsed_data.count('hound-' + breed) > 0
--- home_work/tests/test_yandex.py
+++ home_work/tests/test_yandex.py
@@ -4,11 +4,5 @@ import requests
 def test_response(request_params):
     '''Checking url and response. For valid urls code should be 200, for invalid - 404 otherwise test will fail '''
 
-
-response = requests.get(request_params['url'])
-
-if request_params['status'] == 200:
-    assert response.status_code == int(request_params['status'])
-    print(request_params['status'])
-else:
+    response = requests.get(request_params['url'])
     assert response.status_code == int(request_params['status'])
